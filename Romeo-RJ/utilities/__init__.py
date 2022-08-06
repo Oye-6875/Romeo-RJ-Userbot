fde30913ad9c16e3f6461c20049604e467fe8dee
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2022-08-06 16:56:57 +0530

    Create __init__.py

---
+++ Romeo-RJ/utilities/__init__.py
@@ -0,0 +1,19 @@
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+
+from pymongo import MongoClient
+
+from pyrogram import Client
+
+from Romeo-RJ import config
+
+from ..logger import LOGGER
+
+_mongo_async_ = _mongo_client_(config.MONGO_DB_URL)
+
+_mongo_sync_ = MongoClient(config.MONGO_DB_URL)
+
+mongodb = _mongo_async_.RomeoRJ
+
+pymongodb = _mongo_sync_.RomeoRJ
+
+dbb = _mongo_async_["ROMER"]
