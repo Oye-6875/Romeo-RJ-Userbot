a3eb7555c57a6223d4dd34ff83e919741faf5102
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2022-08-06 19:59:18 +0530

    Create ping.py

---
+++ RomeoRJ/plugins/ping.py
@@ -0,0 +1,33 @@
+import requests
+
+from datetime import datetime
+
+from pyrogram import filters, Client
+
+from RomeoRJ.utilities.misc import SUDOERS
+
+# ping checker
+
+@Client.on_message(filters.command(["ping"], ["/", ".", "!"]) & SUDOERS)
+
+async def ping(Client, message):
+
+    start = datetime.now()
+
+    loda = await message.reply_text("**Romeo_RJ**")
+
+    end = datetime.now()
+
+    mp = (end - start).microseconds / 1000
+
+    await loda.edit_text(f"**😎 Poɴɢ\n»** `{mp} ms`")
+
+__MODULE__ = "Pɪɴɢ"
+
+__HELP__ = f"""
+
+**🔥 Cʜᴇᴄᴋ Yᴏᴜʀ  UsᴇʀBᴏᴛ Pɪɴɢ.**
+
+`.ping` - **Usᴇ Tʜɪs Cᴏᴍᴍᴀɴᴅ Tᴏ Cʜᴇᴄᴋ**
+
+"""
