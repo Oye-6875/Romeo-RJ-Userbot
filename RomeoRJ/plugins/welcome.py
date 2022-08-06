34ca6d5c76c503a7d27f276826f8d092cd23df57
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2022-08-06 18:16:40 +0530

    Create welcome.py

---
+++ RomeoRJ/plugins/welcome.py
@@ -0,0 +1,12 @@
+# Comming Soon 
+
+__MODULE__ = "Wᴇʟᴄᴏᴍᴇ"
+
+__HELP__ = f"""
+
+`.setwelcome [keyword]` **- Sᴇᴛ Cᴜsᴛᴏᴍ Wᴇʟᴄᴏᴍᴇ Mᴇssᴀɢᴇ**
+
+`.clearwelcome` **- Sᴛᴏᴘ & Cʟᴇᴀʀ Wᴇʟᴄᴏᴍᴇ Mᴇssᴀɢᴇ**
+
+"""
+
