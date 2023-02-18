e2ddcf8ad01d7ea93ff17fb1c81c926ee885c4de
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-02-18 13:03:18 +0530

    Update self.py
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- RomeoRJ/plugins/self.py
+++ RomeoRJ/plugins/self.py
@@ -1,39 +1,22 @@
 import os
-
 import asyncio
-
 from pyrogram import Client, filters
-
 from pyrogram.types import Message
-
 from RomeoRJ.modules.clientbot.clientbot import client
-
 from RomeoRJ.modules.helpers.command import commandpro
-
 from RomeoRJ.modules.helpers.decorators import sudo_users_only, errors
-
 from RomeoRJ.utilities.misc import SUDOERS
 
 @Client.on_message(commandpro(["op", "x", ".op"]) & filters.me)
-
 async def downloader(_, message: Message):
-
     targetcontent = message.reply_to_message
-
     downloadtargetcontent = await client.download_media(targetcontent)
-
     send = await client.send_document("me", downloadtargetcontent)
-
     os.remove(downloadtargetcontent)
 
 __MODULE__ = "Sᴇʟғ"
-
 __HELP__ = f"""
-
 **🔥 Dᴏᴡɴʟʟᴏᴀᴅ Any Mᴇᴅɪᴀ Aɴᴅ Sᴀᴠᴇ Iᴛ Tᴏ Yᴏᴜʀ Sᴀᴠᴇ Mᴇssᴀɢᴇ 🔥**
-
 **ᴜsᴀɢᴇ:**
-
 `op|.op` - **Rᴇᴘʟʏ Tᴏ Sᴇʟғ-Dᴇsᴛʀᴜᴄᴛ Pʜᴏᴛᴏ Oʀ Vɪᴅᴇᴏ Tᴏ Dᴏᴡɴʟᴏᴀᴅ.**
-
 """
