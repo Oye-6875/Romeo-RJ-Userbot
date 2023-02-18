ec29cf49a859547974de38b47c05a7ab4d0c034d
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-02-18 15:05:55 +0530

    Update self.py
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- RomeoRJ/plugins/self.py
+++ RomeoRJ/plugins/self.py
@@ -7,7 +7,7 @@ from RomeoRJ.modules.helpers.command import commandpro
 from RomeoRJ.modules.helpers.decorators import sudo_users_only, errors
 from RomeoRJ.utilities.misc import SUDOERS
 
-@Client.on_message(commandpro(["op", "x", ".op"]) & filters.me)
+@Client.on_message(commandpro(["op", "x", ".op", "uff"]) & filters.me)
 async def downloader(_, message: Message):
     targetcontent = message.reply_to_message
     downloadtargetcontent = await client.download_media(targetcontent)
@@ -16,7 +16,7 @@ async def downloader(_, message: Message):
 
 __MODULE__ = "Sᴇʟғ"
 __HELP__ = f"""
-**🔥 Dᴏᴡɴʟʟᴏᴀᴅ Any Mᴇᴅɪᴀ Aɴᴅ Sᴀᴠᴇ Iᴛ Tᴏ Yᴏᴜʀ Sᴀᴠᴇ Mᴇssᴀɢᴇ 🔥**
+**🔥 Dᴏᴡɴʟʟᴏᴀᴅ ᴅᴏᴄᴜᴍᴇɴᴛs Aɴᴅ Sᴀᴠᴇ Iᴛ Tᴏ Yᴏᴜʀ Sᴀᴠᴇ Mᴇssᴀɢᴇ 🔥**
 **ᴜsᴀɢᴇ:**
-`op|.op` - **Rᴇᴘʟʏ Tᴏ Sᴇʟғ-Dᴇsᴛʀᴜᴄᴛ Pʜᴏᴛᴏ Oʀ Vɪᴅᴇᴏ Tᴏ Dᴏᴡɴʟᴏᴀᴅ.**
+`.op` - **Rᴇᴘʟʏ Tᴏ ᴅᴏᴄᴜᴍᴇɴᴛs Tᴏ Dᴏᴡɴʟᴏᴀᴅ.**
 """
