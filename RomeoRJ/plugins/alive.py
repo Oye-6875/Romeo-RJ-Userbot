4358cce07e63b8599b775b6c82eb6e0b4a957f57
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-02-18 12:39:05 +0530

    Update alive.py
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- RomeoRJ/plugins/alive.py
+++ RomeoRJ/plugins/alive.py
@@ -1,29 +1,16 @@
-# RomeoRJ
-
 import asyncio
-
 from pyrogram import *
-
 from pyrogram.types import *
-
 from RomeoRJ.modules.helpers.basics import edit_or_reply
-
 from RomeoRJ.modules.helpers.filters import command
-
 from RomeoRJ.utilities.misc import SUDOERS
 
 @Client.on_message(command(["alive"]) & SUDOERS)
-
 async def mother_chod(client: Client, message: Message):
-
     await edit_or_reply(message, "**I'm alive 😎**")
 
 __MODULE__ = "Aʟɪᴠᴇ"
-
 __HELP__ = f"""
-
 **🔥 Tᴇsᴛ Yᴏᴜʀ Bᴏᴛ Wᴏʀᴋɪɴɢ Oʀ Nᴏᴛ.**
-
 `.alive` - **Usᴇ Tʜɪs Cᴏᴍᴍᴀɴᴅ Tᴏ Cʜᴇᴄᴋ**
-
 """
