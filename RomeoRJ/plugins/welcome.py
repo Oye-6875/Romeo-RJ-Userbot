775d36cd2325bfa6b850b78289b1353a25579534
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-02-18 13:08:08 +0530

    Update welcome.py
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- RomeoRJ/plugins/welcome.py
+++ RomeoRJ/plugins/welcome.py
@@ -1,12 +1,8 @@
 # Comming Soon 
 
 __MODULE__ = "Wᴇʟᴄᴏᴍᴇ"
-
 __HELP__ = f"""
-
 `.setwelcome [keyword]` **- Sᴇᴛ Cᴜsᴛᴏᴍ Wᴇʟᴄᴏᴍᴇ Mᴇssᴀɢᴇ**
-
 `.clearwelcome` **- Sᴛᴏᴘ & Cʟᴇᴀʀ Wᴇʟᴄᴏᴍᴇ Mᴇssᴀɢᴇ**
-
 """
 
