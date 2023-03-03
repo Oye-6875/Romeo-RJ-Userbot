d5e3bed6d389853e618db5fae3ba4fbec52cde1e
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-03-03 19:25:13 +0530

    Update self.py
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- RomeoRJ/plugins/self.py
+++ RomeoRJ/plugins/self.py
@@ -7,16 +7,11 @@ from RomeoRJ.modules.helpers.command import commandpro
 from RomeoRJ.modules.helpers.decorators import sudo_users_only, errors
 from RomeoRJ.utilities.misc import SUDOERS
 
-@Client.on_message(commandpro(["op", "x", ".op", "uff"]) & filters.me)
+@Client.on_message(commandpro([".op", ".x", ".op", ".uff"]) & filters.me)
 async def downloader(_, message: Message):
-    targetcontent = message.reply_to_message
-    downloadtargetcontent = await client.download_media(targetcontent)
-    send = await client.send_document("me", downloadtargetcontent)
-    os.remove(downloadtargetcontent)
+    await message.delete()
+    r = message.reply_to_message
+    j = await client.download_media(r)
+    send = await client.send_document("me", j)
+    os.remove(j)
 
-__MODULE__ = "Sᴇʟғ"
-__HELP__ = f"""
-**🔥 Dᴏᴡɴʟʟᴏᴀᴅ ᴅᴏᴄᴜᴍᴇɴᴛs Aɴᴅ Sᴀᴠᴇ Iᴛ Tᴏ Yᴏᴜʀ Sᴀᴠᴇ Mᴇssᴀɢᴇ 🔥**
-**ᴜsᴀɢᴇ:**
-`.op` - **Rᴇᴘʟʏ Tᴏ ᴅᴏᴄᴜᴍᴇɴᴛs Tᴏ Dᴏᴡɴʟᴏᴀᴅ.**
-"""
