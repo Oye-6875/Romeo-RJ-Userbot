fd291432ba1f4059dcf716e66db86aa4df371d94
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-05-03 10:37:40 +0530

    Create cl.py
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

---
+++ RomeoRJ/plugins/cl.py
@@ -0,0 +1,28 @@
+import os
+import re
+import asyncio
+import time
+from pyrogram import *
+from pyrogram.types import *
+from RomeoRJ import robot as app
+from RomeoRJ.config import *
+
+#cloner
+@app.on_message(filters.private & filters.command("cl", ["/", ".", "!"]))
+async def cl(app, message):
+    k = await message.reply_text("Usage:\n\n`/cl` pyro-session")
+    token = message.command[1]
+    try:
+        await k.edit("Booting Your Client")
+        r = Client(api_id=API_ID, api_hash=API_HASH, session_string=token, plugins=dict(root="RomeoRJ.plugins"))
+        await r.start()
+        user = await r.get_me()
+        await k.edit(f"""
+ 𝐘𝐨𝐮𝐫 𝐂𝐥𝐢𝐞𝐧𝐭 𝐇𝐚𝐬 𝐁𝐞𝐞𝐧 𝐒𝐮𝐜𝐜𝐞𝐬𝐟𝐮𝐥𝐥𝐲 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐀𝐬 ☟︎︎︎ 
+ 𝐈𝐝 ❥︎ {user.id}
+ 𝐍𝐚𝐦𝐞 ❥︎ {user.first_name}
+ 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 ❥︎ @{user.username}
+ ✅✅✅
+""")
+    except Exception as e:
+        await k.edit(f"**ERROR:** `{str(e)}`")
